49abfe441b5e8e71e1a772064de1480ccbd3ed3f
Kuroshiel <alifianfikry@gmail.com>
2024-01-16 20:47:18 +0700

    Learn Embed file ke String

---
+++ embed_test.go
@@ -0,0 +1,19 @@
+package belajargolangembed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+// Embed file ke String
+
+//go:embed version.txt
+var version string
+
+//go:embed version.txt
+var version2 string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+}
