29341c65d72a7699e8afa6d0bc0de16c0028f909
Kuroshiel <alifianfikry@gmail.com>
2024-01-17 11:48:52 +0700

    Learn Embed Multiple Files

--- embed_test.go
+++ embed_test.go
@@ -1,6 +1,7 @@
 package belajargolangembed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -31,4 +32,22 @@ func TestByte(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Embed Multiple Files
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
 }
